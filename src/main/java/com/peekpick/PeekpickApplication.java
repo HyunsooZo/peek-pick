b6efa799128756aa58a6234a24f5055ba80433ea
bzhs1992 <bzhs1992@icloud.com>
2025-04-01 21:35:40 +0900

    init: initial project setup

---
+++ src/main/java/com/peekpick/PeekpickApplication.java
@@ -0,0 +1,13 @@
+package com.peekpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeekpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeekpickApplication.class, args);
+    }
+
+}
